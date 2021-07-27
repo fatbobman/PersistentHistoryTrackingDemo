5eb53264b162841cfa07955926ce2e25cfa0d475
Yang Xu <xuyang@me.com>
2021-07-27 08:44:09 +0800

    Initial Commit

---
+++ PersistentHistoryTrackingDemo/PersistentHistoryTrackingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersistentHistoryTrackingDemoApp.swift
+//  PersistentHistoryTrackingDemo
+//
+//  Created by Yang Xu on 2021/7/27.
+//
+
+import SwiftUI
+
+@main
+struct PersistentHistoryTrackingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
